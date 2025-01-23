bcd420d4b985acc483e77381bec7d1037bd91c06
Vitorio Micheletto <vitoriomicheletto@gmail.com>
2025-01-22 23:29:04 -0300

    Project created

---
+++ src/test/java/com/devmicheletto/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmicheletto.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
